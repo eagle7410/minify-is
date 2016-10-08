741cfce4b83b120de3a92c8bf0c95cc9656708f5
Igor Stcherbina <orel1939@mail.ru>
2016-10-08 19:50:33 +0100

    Fix replace with $.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /**
  * Created by igor on 05.10.16.
  */
-module.exports = require(`./lib/${process.env.NODE_ENV === 'dev' ? 'prod/' : ''}minify-js`);
+module.exports = require(`./lib/${process.env.NODE_ENV !== 'dev' ? 'prod/' : ''}minify-js`);
